ff6a8fc6731f6078a7b68e1cdfd953faefada51e
Majki4306 <wwsis123362@wp.pl>
2016-04-21 23:44:53 +0200

    Zatwierdzenie zmian - myBranch

--- Projekt_gitTest/main.cpp
+++ Projekt_gitTest/main.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 
 int main(){
+	// *** test ***
 	char *str = new char[15];
 	for(int i = 0;i<10;++i){
 		itoa(i,str,10);
 		cout<<str<<endl;
 	}
 	cout<<"Hello world!"<<endl;
+	// *** Dodatkowy kod dla galezi myBranch ***
 	_getch();
 }
